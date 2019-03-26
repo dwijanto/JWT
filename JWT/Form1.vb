852c1cd66a041990bde7ab400323eef7a3a48632
dwijanto <dwijanto@yahoo.com>
2019-03-26 15:06:23 +0800

    Initial Project
    Initial Project

---
+++ JWT/Form1.vb
@@ -0,0 +1,34 @@
+﻿Imports System.IdentityModel
+Imports System.Security
+Imports System.Text
+Imports Microsoft.IdentityModel.Tokens
+Imports System.IdentityModel.Tokens.Jwt
+
+Public Class Form1
+   
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim key = "LieDwiJantoJohanLieDwiJantoJohan"
+        Dim securitykey = New Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+        Dim credentials = New Microsoft.IdentityModel.Tokens.SigningCredentials(securitykey, SecurityAlgorithms.HmacSha256)
+        Dim header = New JwtHeader(credentials)
+
+        Dim payload = New JwtPayload From {{"role", "todo_user"}}
+        Dim secToken = New JwtSecurityToken(header, payload)
+        Dim handler = New JwtSecurityTokenHandler()
+
+        Dim tokenString = handler.WriteToken(secToken)
+        MessageBox.Show(tokenString)
+        Dim token = handler.ReadJwtToken(tokenString)
+        MessageBox.Show(token.Payload.First().Value)
+
+    End Sub
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+End Class
---
+++ JWT/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("JWT")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("JWT")> 
+<Assembly: AssemblyCopyright("Copyright ©  2019")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("689ab86c-dd3b-433c-9a6d-3ec51acfc41f")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
